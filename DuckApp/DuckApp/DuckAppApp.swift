e52fc0eee16fa3b1e008b0f29d95e6b34f8d3cf9
Mariana <hellodev.mm@gmail.com>
2026-05-01 19:56:02 +0100

    navigation: Router, tabBar, navigationPath, navigationStack, navigationLinks

---
+++ DuckApp/DuckApp/DuckAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DuckAppApp.swift
+//  DuckApp
+//
+//  Created by Mariana Mendes on 01/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct DuckAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
